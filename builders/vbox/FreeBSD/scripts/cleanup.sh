a5bf3a2d1f4b0ed6e99434f6e1a11821887c3a0d
N-Reyna <97756167+N-Reyna@users.noreply.github.com>
2022-01-14 12:44:48 -0600

    Add files via upload

---
+++ builders/vbox/FreeBSD/scripts/cleanup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# Disable root logins
+sed -i '' -e 's/^PermitRootLogin yes/#PermitRootLogin no/' /etc/ssh/sshd_config
+
+# Purge files we no longer need
+rm -rf /boot/kernel.old
+rm -f /etc/ssh/ssh_host_*
+rm -f /root/*.iso
+rm -f /root/.vbox_version
+rm -rf /tmp/*
+rm -rf /var/db/freebsd-update/files/*
+rm -f /var/db/freebsd-update/*-rollback
+rm -rf /var/db/freebsd-update/install.*
+rm -f /var/db/pkg/repo-*.sqlite
+rm -rf /var/log/*
