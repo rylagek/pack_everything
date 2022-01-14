c9f5d376f6af0e1607444d05359874dbdfe212cd
alangston24 <97756191+alangston24@users.noreply.github.com>
2022-01-14 12:42:32 -0600

    Create vbox.sh

---
+++ builders/vbox/Arch/Scripts/vbox.sh
@@ -0,0 +1,13 @@
+pacman -Sy
+pacman -S openssh
+y
+echo | ...
+systemctl start sshd
+systemctl enable sshd
+ssh-keygen -t rsa
+useradd vbox
+passwd vbox
+vbox
+vbox
+mkdir /home/vbox/.ssh/authorized_keys
+echo "key" > /home/vbox/.ssh/authorized_keys
