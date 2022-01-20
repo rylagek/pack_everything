3dd1b3f7d7b39736997b9c01c4d3732e847563a5
alangston24 <97756191+alangston24@users.noreply.github.com>
2022-01-20 09:48:24 -0600

    Update setup.ps1

--- builders/vbox/Windows Server 2019/setup.ps1
+++ builders/vbox/Windows Server 2019/setup.ps1
@@ -81,15 +81,3 @@ function prompt {
 
 New-Item $PROFILE -ItemType File -Force
 Set-Content -Path $PROFILE -Value $psprofile
-
-Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
-Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
-Start-Service sshd
-Set-Service -Name sshd -StartupType 'Automatic'
-
-if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
-    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
-    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-} else {
-    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
-}
