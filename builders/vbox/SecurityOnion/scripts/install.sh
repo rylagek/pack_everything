76f5216dacdad47a6f7c012811876ba0ef10c808
Andre' Chris Coryea-Robinson <andrerobcs@gmail.com>
2022-02-08 21:35:42 -0600

    Removed "sudo" calls from inside install.sh

--- builders/vbox/SecurityOnion/scripts/install.sh
+++ builders/vbox/SecurityOnion/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Installing via so-setup"
 pwd
-echo "automation" | sudo -S yum -y install screen
-echo "automation" | sudo -S screen -DmS installation SecurityOnion/setup/so-setup iso ucwt-iso
+env
+echo "Installing via so-setup"
+SecurityOnion/setup/so-setup iso ucwt-iso
 echo "Finished installing via so-setup"
