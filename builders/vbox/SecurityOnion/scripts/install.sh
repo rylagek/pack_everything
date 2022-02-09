47558403830170862d0303df065ebb7cd548bd68
Andre' Chris Coryea-Robinson <andrerobcs@gmail.com>
2022-02-08 20:59:10 -0600

    Playing with the install script to try and fix permissions

--- builders/vbox/SecurityOnion/scripts/install.sh
+++ builders/vbox/SecurityOnion/scripts/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "Installing via so-setup"
-sudo SecurityOnion/setup/so-setup iso ucwt-iso
+pwd
+echo "automation" | sudo -S yum -y install screen
+echo "automation" | sudo -S screen -DmS installation SecurityOnion/setup/so-setup iso ucwt-iso
 echo "Finished installing via so-setup"
